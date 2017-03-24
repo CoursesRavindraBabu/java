daa600e278eeb55eb0525314f82c52c9301d81bb
satheesh586 <dannuri.satheesh@gmail.com>
2017-03-24 22:09:36 +0530

    Operators solutions added

---
+++ Operators/Question8/Question8.java
@@ -0,0 +1,38 @@
+package Operators.Question8;
+
+/*
+(Print a table) Write a program that displays the following table. Cast floatingpoint
+numbers into integers.
+a 		b 		pow(a, b)
+1 		2 		1
+2 		3 		8
+3 		4 		81
+4 		5 		1024
+5 		6 		15625
+*/
+public class Question8 {
+	public static void main(String[] args) {
+		float a, b;
+		System.out.println("a        b        pow(a, b)");
+		a = 1;
+		b = 2;
+		System.out.println((int)a + "        " + (int)b + 
+			"        " + (int)Math.pow(a, b));
+		a++;
+		b++;
+		System.out.println((int)a + "        " + (int)b + 
+			"        " + (int)Math.pow(a, b));
+		a++;
+		b++;
+		System.out.println((int)a + "        " + (int)b + 
+			"        " + (int)Math.pow(a, b));
+		a++;
+		b++;
+		System.out.println((int)a + "        " + (int)b + 
+			"        " + (int)Math.pow(a, b));
+		a++;
+		b++;
+		System.out.println((int)a + "        " + (int)b + 
+			"        " + (int)Math.pow(a, b));
+	}
+}
